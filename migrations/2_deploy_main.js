699fcd67645b42babb6cc40824a79e0c70f9486d
ana0 <sarah.anne.friend@gmail.com>
2022-06-14 12:01:08 +0200

    Set controller for dev

--- migrations/2_deploy_main.js
+++ migrations/2_deploy_main.js
@@ -1,7 +1,7 @@
 const Wildcards = artifacts.require('Wildcards');
 
 module.exports = async function (deployer, network, accounts) {
-  return deployer.deploy(Wildcards, accounts[0],52, 'https://isthisa.computer/api/wildcards/', 'Wildcards', 'WILD', { from: accounts[0] })
+  return deployer.deploy(Wildcards, '0x4E34c31A799f7caf7d2378C68dD171B2A0B58817', 52, 'https://isthisa.computer/api/wildcards/', 'Wildcards', 'WILD', { from: accounts[0] })
     .then(async (wildcards) => {
       //await lifeforms.setContractURI('https://isthisa.computer/ipfs/QmVDJwDuRv9PsRvPGxqengVCPawCVXdmdda74wEbGFC1ZQ/lifeforms-contract.json');
     });
